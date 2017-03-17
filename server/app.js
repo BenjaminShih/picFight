c42781deb32c4363c6dba1781603e2b2b5f87ebf
sjn <sjn@xkeshi.com>
2017-03-17 10:17:39 +0800

    fix: 可以显示前端页面定义的全部图片

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const indexPath = '../client/index.html'
 app.use(router.routes())
 	.use(router.allowedMethods())
 
+// 日志相关
 app.use(async (ctx, next) => {
 	const start = new Date();
 	await next();
@@ -17,13 +18,15 @@ app.use(async (ctx, next) => {
 	console.log(`${ctx.method}${ctx.url} - ${ms}ms`);
 })
 
+// 返回html
 app.use (ctx => {
 	let filePath = path.join(__dirname, indexPath);
 	let content = fs.readFileSync(filePath, 'binary');
 	ctx.body = content;
 })
 
-router.get('/client/assets/pics/qiaoqiaohua.jpg', async (ctx) => {
+// 接收图片路径返回图片
+router.get('/client/assets/pics/:pic', async (ctx) => {
 	let picPath = path.join(__dirname, ".." + ctx.request.url)
 	let picContent = fs.readFileSync(picPath, 'binary');
 	ctx.status = 200;
