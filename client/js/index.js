57f94ac9150026862c8d89094587c60eff0f5c15
sjn <sjn@xkeshi.com>
2017-03-21 19:46:44 +0800

    feat: 增加webpack打包功能，抽离html中的js, 可以使用npm包编写前端页面了！

---
+++ client/js/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+
+var app = new Vue({
+	el: '#app',
+	data: function() {
+		return{
+			picArr : []
+		}
+	},
+	mounted: function() {
+		// 初始化加载数据库中的图片
+		this.loadPics();
+	},
+	methods: {
+		// 清空输入框
+		clearPicText: function(item) {
+			item.text = '';
+		},
+		// 上传图片
+		upload: function(e) {
+			let formData = new FormData();
+			formData.append('uploadfile', e.target.files[0]);
+			console.log('formData', formData)
+			this.$http.post('/upload', formData)
+			.then(function (response) {
+			    console.log('upload success!');
+			    this.loadPics()
+			}, function(err) {
+				console.log('upload err -', err)
+			})
+		},
+		// 加载图片
+		loadPics: function() {
+			this.$http.post('/pics', {}).then(function(res) {
+				let arr =  res.data
+				if(Array.isArray(arr)) {
+					this.picArr = arr;
+				}
+			})
+		},
+	}
+})
