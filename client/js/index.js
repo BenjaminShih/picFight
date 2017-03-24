7403262fbd1fae3d5b626dde79abfbc364084592
sjn <sjn@xkeshi.com>
2017-03-24 13:17:25 +0800

    feat: 因mdl和vue和js兼容问题，删除mdl，UI库改为iview

--- client/js/index.js
+++ client/js/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from './app.vue'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 
 Vue.use(VueResource)
+Vue.use(iView)
 
 var app = new Vue({
 	el: '#app',
