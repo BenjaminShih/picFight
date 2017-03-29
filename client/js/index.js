fbd83111df1609c9775fff4be3cb2dfd3b8d9a04
benjaminShih <576864911@qq.com>
2017-03-29 16:53:08 +0800

    feat: 加入vue-router

--- client/js/index.js
+++ client/js/index.js
@@ -1,13 +1,22 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
 import App from './app.vue'
 import iView from 'iview'
+import routes from './routes'
 import 'iview/dist/styles/iview.css'
 
 Vue.use(VueResource)
+Vue.use(VueRouter)
 Vue.use(iView)
 
+const router = new VueRouter({
+	mode: 'history',
+	routes
+})
+
 var app = new Vue({
+	router,
 	el: '#app',
 	components: {
 		App: App
---
+++ client/js/routes.js
@@ -0,0 +1,8 @@
+import layout from './layout.vue'
+
+const routes = [{
+	path: '/',
+	component: layout
+}]
+
+export default routes
